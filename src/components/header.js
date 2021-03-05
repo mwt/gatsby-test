b0331d6f162acff1713245358c3b455ce96c0c26
Matthew Wildrick Thomas <matthew@mwt.me>
2021-03-05 00:47:13 +0000

    fix video

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { LastWord } from "../buildtime-scripts";
 
-export default function ComponentNav(props) {
+export default function ComponentHeader(props) {
     return (
         <header id="top" class="masthead text-center dark-bg d-flex">
-            <video autoplay muted loop id="bg-video" poster="{{ page.video.pre }}/{{ page.video.post }}.jpg">
-                <source src="{{ page.video.pre }}/{{ page.video.transform }}/{{ page.video.post }}.webm" type="video/webm" />
-                <source src="{{ page.video.pre }}/{{ page.video.transform }}/{{ page.video.post }}.mp4" type="video/mp4" />
+            <video autoPlay muted loop id="bg-video" poster="https://res.cloudinary.com/mariabetto/video/upload/v1612134740/production_ID_5200349_xsjpw2.jpg">
+                <source src="https://res.cloudinary.com/mariabetto/video/upload/ac_none,c_scale,q_auto:good,so_2,vc_auto,w_1600/v1612134740/production_ID_5200349_xsjpw2.webm" type="video/webm" />
+                <source src="https://res.cloudinary.com/mariabetto/video/upload/ac_none,c_scale,q_auto:good,so_2,vc_auto,w_1600/v1612134740/production_ID_5200349_xsjpw2.mp4" type="video/mp4" />
             </video>
 
             <div class="video-overlay header-text">
